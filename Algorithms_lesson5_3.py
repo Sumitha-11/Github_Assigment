e4831ad9013059c943969f30011e9ccf0a8ac3d8
sumitha <sundaresansumitha@gmail.com>
2022-01-25 14:00:41 -0800

    Insertion sort in descending order

---
+++ Algorithms_lesson5_3.py
@@ -0,0 +1,16 @@
+
+"""Insertion Sort
+Implement the insertion sort algorithm that is sorting in descending order.
+"""
+
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1
+
+        while j >= 0 and key > arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+print(insertion_sort([1,2,3,4,5,6,7,8]))
